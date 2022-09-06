8dc138ff5cd6a2980c02ea2f38b55dabe74c4c5e
ll647 <112925286+ll647@users.noreply.github.com>
2022-09-06 14:24:07 +0800

    Add files via upload

---
+++ home002.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	printf("«Î ‰»Îa,b:");
+	scanf_s("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("a=%d\nb=%d", a, b);
+}
